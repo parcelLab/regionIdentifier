dbb69b303b9d8dc9e386cf4f6eb773aa08312bef
oscar <oscar-rreyes1@hotmail.com>
2016-05-30 19:32:01 +0200

    Initial commit.

---
+++ test/tests.js
@@ -0,0 +1,227 @@
+const RegionIdentifier = require('../lib/region');
+var identifier = new RegionIdentifier();
+const fs = require('fs');
+identifier.get('AT', '2151', console.log);
+identifier.get('ARG', '58040', console.log);
+identifier.get('MX', '35078', console.log);
+
+// var parses = {
+//  	'Alsace-Champagne-Ardenne-Lorraine': 'FR-A',
+//  	'Aquitaine-Limousin-Poitou-Charentes': 'FR-B',
+//  	'Auvergne-Rhône-Alpes': 'FR-C',
+//  	'Bourgogne-Franche-Comté':'FR-D',
+// 	'Bretagne': 'FR-E',
+// 	'Île-de-France': 'FR-J',
+// 	'Corse':'FR-H',
+// 	'Normandy': 'FR-Q',
+// 	'Centre': 'FR-F',
+// 	'Languedoc-Roussillon-Midi-Pyrénées': 'FR-K',
+// 	'Nord-Pas-de-Calais-Picardie': 'FR-O',
+// 	'Pays de la Loire':'FR-R',
+// 	'Provence-Alpes-Côte d\'Azur': 'FR-U',
+// };
+
+// var parses = {
+// 	'Abruzzi': 'IT-65',
+// 	'Basilicata': 'IT-77',
+// 	'Calabria': 'IT-78',
+// 	'Campania': 'IT-72',
+// 	'Emilia-Romagna':'IT-45',
+// 	'Friuli-Venezia Giulia':'IT-36',
+// 	'Lazio': 'IT-62',
+// 	'Liguria': 'IT-42',
+// 	'Lombardia': 'IT-25',
+// 	'Marche': 'IT-57',
+// 	'Molise': 'IT-67',
+// 	'Piemonte': 'IT-21',
+// 	'Puglia': 'IT-75',
+// 	'Sardegna': 'IT-88',
+// 	'Sicilia': 'IT-82',
+// 	'Toscana': 'IT-52',
+// 	'Trentino-Alto Adige': 'IT-32',
+// 	'Umbria': 'IT-55',
+// 	'Valle D\'Aosta': 'IT-23',
+// 	'Veneto': 'IT-34',
+// };
+
+// var parses = {
+// 	'Provincie Drenthe': 'NL-DR',
+// 	'Provincie Friesland':'NL-FR',
+// 	'Provincie Flevoland': 'NL-FL',
+// 	'Provincie Gelderland': 'NL-GE',
+// 	'Provincie Groningen': 'NL-GR',
+// 	'Provincie Limburg': 'NL-LI',
+// 	'Provincie Noord-Brabant': 'NL-NB',
+// 	'Provincie Noord-Holland': 'NL-NH',
+// 	'Provincie Utrecht': 'NL-UT',
+// 	'Provincie Overijssel':' NL-OV',
+// 	'Provincie Zeeland': 'NL-ZE',
+// 	'Provincie Zuid-Holland':'NL-ZH',
+// };
+
+var parses = {
+	'AG': 'CH-AG',
+	'AR': 'CH-AR',
+	'AI': 'CH-AI',
+	'BL': 'CH-BL',
+	'BS': 'CH-BS',
+	'BE': 'CH-BE',
+	'FR': 'CH-FR',
+	'GE': 'CH-GE',
+	'GL': 'CH-GL',
+	'GR': 'CH-GR',
+	'JU': 'CH-JU',
+	'LU': 'CH-LU',
+	'NE': 'CH-NE',
+	'NW': 'CH-NW',
+	'OW': 'CH-OW',
+	'SG': 'CH-SG',
+	'SH': 'CH-SH',
+	'SZ': 'CH-SZ',
+	'SO': 'CH-SO',
+	'TG': 'CH-TG',
+	'TI': 'CH-TI',
+	'UR': 'CH-UR',
+	'VS': 'CH-VS',
+	'VD': 'CH-VD',
+	'ZG': 'CH-ZG',
+	'ZH': 'CH-ZH',
+};
+
+fs.readFile('./CH.txt', function(err, data) {
+	data = data.toString().split('\n');
+	var result = [];
+	var haveIt= [];
+	data.forEach(function(code) {
+		code = code.split('	');
+		var parse = parses[code[4]];
+		if(!code[1] || !parse) return console.log(code[4]);
+		var postalCode = code[1].match(/[0-9]{4,}/)[0];
+		var found = haveIt.filter((e) => {return e === postalCode; })[0];
+		if(found) return;
+		haveIt.push(postalCode);
+		result.push({
+			low: postalCode,
+			// high: postalCode,
+			region: parse,
+		});
+	});
+
+	var bounds = {
+		'CH-AG': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-AR': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-AI': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-BL': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-BS': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-BE': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-FR': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-GE': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-GL': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-GR': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-JU': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-LU': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-NE': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-NW': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-OW': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-SG': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-SH': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-SZ': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-SO': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-TG': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-TI': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-UR': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-VS': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-VD': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-ZG': {
+			min: 999999,
+			max: 0,
+		},
+		'CH-ZH': {
+			min: 999999,
+			max: 0,
+		},
+	};
+
+	result.forEach(function(res) {
+		var bound = bounds[res.region];
+		if(res.region === 'undefined' || !bound) return;
+		if(res.low < bound.min)
+			bound.min = res.low;
+		if(res.high > bound.max)
+			bound.max = res.high;
+	});	
+
+	fs.writeFile('./regions/CHE.json', JSON.stringify(result), console.log);
+});
