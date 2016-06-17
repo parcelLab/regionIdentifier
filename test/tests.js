09ca0d79893e9d77c0b6718fa669b9695011f3f4
Julian Krenge <julian@parcellab.com>
2016-06-17 14:02:10 +0200

    Fixed region detection for GBR
    GBR zip codes can be of the form with two strings separated by a space,
    these are now being parsed correctly.

--- test/tests.js
+++ test/tests.js
@@ -2,82 +2,69 @@ const RegionIdentifier = require('../lib/region');
 const expect = require('chai').expect;
 var identifier = new RegionIdentifier('<API KEY>');
 
-var tests = [
- {
-  name:'AUT', 
- 	zp: '1999',
- 	result: 'AT-9',
- },
- {
-  name:'Austria', 
- 	zp: '1999',
- 	result: 'AT-9',
- },
- {
-  name:'Deutschland', 
- 	zp: '6578',
- 	result: 'DE-TH',
- },
- {
-  name:'BEL', 
- 	zp: '1701',
- 	result: 'BE-VLG',
- },
- {
-  name:'CHE', 
- 	zp: '2333',
- 	result: 'CH-BE',
- },
- {
-  name:'DEU', 
- 	zp: '6578',
- 	result: 'DE-TH',
- },
- {
-  name:'ESP', 
- 	zp: '49000',
- 	result: 'ES-ZA',
- },
- {
-  name:'FRA', 
- 	zp: '25090',
- 	result: 'FR-D',
- },
- {
-  name:'FR', 
- 	zp: '25090',
- 	result: 'FR-D',
- },
- {
-  name:'GBR', 
- 	zp: 'ZE43',
- 	result: 'GB-SCT',
- },
- {
-  name:'ITA', 
- 	zp: '67033',
- 	result: 'IT-65',
- },
- {
-  name:'NLD', 
- 	zp: '3541',
- 	result: 'NL-UT',
- }
-];
+var tests = [{
+  name: 'AUT',
+  zp: '1999',
+  result: 'AT-9',
+}, {
+  name: 'Austria',
+  zp: '1999',
+  result: 'AT-9',
+}, {
+  name: 'Deutschland',
+  zp: '6578',
+  result: 'DE-TH',
+}, {
+  name: 'BEL',
+  zp: '1701',
+  result: 'BE-VLG',
+}, {
+  name: 'CHE',
+  zp: '2333',
+  result: 'CH-BE',
+}, {
+  name: 'DEU',
+  zp: '6578',
+  result: 'DE-TH',
+}, {
+  name: 'ESP',
+  zp: '49000',
+  result: 'ES-ZA',
+}, {
+  name: 'FRA',
+  zp: '25090',
+  result: 'FR-D',
+}, {
+  name: 'FR',
+  zp: '25090',
+  result: 'FR-D',
+}, {
+  name: 'GBR',
+  zp: 'PA13 4PS',
+  result: 'GB-SCT',
+}, {
+  name: 'ITA',
+  zp: '67033',
+  result: 'IT-65',
+}, {
+  name: 'NLD',
+  zp: '3541',
+  result: 'NL-UT',
+}];
 
 describe('Beggining Test.', function () {
 
   describe('Testing get function', function () {
-    
-  	tests.forEach(function(test) {
-  		identifier.get(test.name, test.zp, function(err, region) {
-  			var text = 'Validating result for: ' + test.name + ' with zip code:' +
-  			 test.zp + ' result: ' + region;
-  			it(text, function () {
-      		expect(region).to.be.equals(test.result);
-      		expect(err).to.be.null;
-    		});		
-  		});
-  	});
- 	});
+
+    tests.forEach(function (test) {
+      identifier.get(test.name, test.zp, function (err, region) {
+        var text = 'Validating result for: ' + test.name + ' with zip code:' +
+          test.zp + ' result: ' + region;
+        it(text, function () {
+          expect(region).to.be.equals(test.result);
+          expect(err).to.be.null;
+        });
+      });
+    });
+  });
 });
