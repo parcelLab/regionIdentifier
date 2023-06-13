d0a4dea535260acf556e799787c785d3c39327cd
Julian Krenge <julian@parcellab.com>
2023-06-13 13:39:17 +0200

    feat: added territories to USA

--- update/usa.js
+++ update/usa.js
@@ -2,32 +2,39 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
-execSync('wget -P update https://download.geonames.org/export/zip/US.zip')
-execSync('unzip update/US.zip -d update')
+const result = []
 
-const data = fs.readFileSync(path.join(__dirname, 'US.txt'), 'utf8')
+const codes = ['US', 'AS', 'FM', 'GU', 'MH', 'MP', 'PR', 'VI']
 
-const agg = {}
-for (const line of data.split('\n')) {
-  const [, zip, , , state] = line.split('\t')
-  if (state) {
-    const region = `US-${state}`
-    if (!agg[region]) {
-      agg[region] = [parseInt(zip, 10)]
-    } else {
-      agg[region].push(parseInt(zip, 10))
+for (const code of codes) {
+  execSync(`wget -P update https://download.geonames.org/export/zip/${code}.zip`)
+  execSync(`unzip -o update/${code}.zip -d update`)
+
+  const data = fs.readFileSync(path.join(__dirname, code + '.txt'), 'utf8')
+
+  const agg = {}
+  for (const line of data.split('\n')) {
+    const [rawCountry, zip, , , rawState] = line.split('\t')
+    const state = rawCountry === 'US' ? rawState : rawCountry
+    if (state) {
+      const region = `US-${state}`
+      if (!agg[region]) {
+        agg[region] = [parseInt(zip, 10)]
+      } else {
+        agg[region].push(parseInt(zip, 10))
+      }
     }
   }
+
+  for (const region in agg) {
+    result.push({
+      region,
+      list: agg[region]
+    })
+  }
+
+  fs.unlinkSync(path.join(__dirname, code + '.txt'))
+  fs.unlinkSync(path.join(__dirname, code + '.zip'))
 }
 
-const result = []
-for (const region in agg) {
-  result.push({
-    region,
-    list: agg[region]
-  })
-}
-
-fs.writeFileSync(path.join(__dirname, '..', 'regions', 'USA.json'), JSON.stringify(result))
-fs.unlinkSync(path.join(__dirname, 'US.txt'))
-fs.unlinkSync(path.join(__dirname, 'US.zip'))
+fs.writeFileSync(path.join(__dirname, '..', 'regions', 'USA.json'), JSON.stringify(result, null, 2))
