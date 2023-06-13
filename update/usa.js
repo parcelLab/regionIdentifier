cccf2c4261e7e748fb4b185a7e870349b8b8ae12
Julian Krenge <julian@parcellab.com>
2023-06-13 13:48:23 +0200

    feat: auto-update for US (#11)
    * feat: auto-update for US
    
    * fix: faulty mapping of sub-strings
    
    * feat: added territories to USA
    
    * chore: update gitignore
    
    * chore: add test action
    
    * Revert "chore: add test action"
    
    This reverts commit dfe0d27782a2e8cff2ba6a71a16ea48d48d01811.

---
+++ update/usa.js
@@ -0,0 +1,40 @@
+const { execSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+const result = []
+
+const codes = ['US', 'AS', 'FM', 'GU', 'MH', 'MP', 'PR', 'VI']
+
+for (const code of codes) {
+  execSync(`wget -P update https://download.geonames.org/export/zip/${code}.zip`)
+  execSync(`unzip -o update/${code}.zip -d update`)
+
+  const data = fs.readFileSync(path.join(__dirname, code + '.txt'), 'utf8')
+
+  const agg = {}
+  for (const line of data.split('\n')) {
+    const [rawCountry, zip, , , rawState] = line.split('\t')
+    const state = rawCountry === 'US' ? rawState : rawCountry
+    if (state) {
+      const region = `US-${state}`
+      if (!agg[region]) {
+        agg[region] = [parseInt(zip, 10)]
+      } else {
+        agg[region].push(parseInt(zip, 10))
+      }
+    }
+  }
+
+  for (const region in agg) {
+    result.push({
+      region,
+      list: agg[region]
+    })
+  }
+
+  fs.unlinkSync(path.join(__dirname, code + '.txt'))
+  fs.unlinkSync(path.join(__dirname, code + '.zip'))
+}
+
+fs.writeFileSync(path.join(__dirname, '..', 'regions', 'USA.json'), JSON.stringify(result, null, 2))
