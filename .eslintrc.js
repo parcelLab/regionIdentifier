caca3f8b552c1b57a236659f86e4419eda1f13ed
Carlos <835129+cvscarlos@users.noreply.github.com>
2023-07-17 08:39:47 +0200

    reduce changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,6 @@ module.exports = {
   extends: ["@parcellab/eslint-config/base"],
   rules: {
     "comma-dangle": ["error", "always-multiline"],
-    "eqeqeq": "error",
-    "for-direction": "error",
-    "no-sequences": "error",
-    "no-undef": "error",
-    "no-unused-vars": "error",
-    "no-use-before-define": "error",
     "prefer-const": "error",
     "quote-props": ["error", "consistent-as-needed"],
     "semi": ["error", "always"],
